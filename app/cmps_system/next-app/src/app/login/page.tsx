3e51587adcf30c17da6fe2ee1ef5466997bb4877
duyiyang <112981522+duyiyang@users.noreply.github.com>
2024-06-21 00:05:08 +0000

    finish login page

--- app/cmps_system/next-app/src/app/login/page.tsx
+++ app/cmps_system/next-app/src/app/login/page.tsx
@@ -7,8 +7,8 @@ import InputGroup from 'react-bootstrap/InputGroup';
 export default function Home() {
     return (
         <div className="tw-grid tw-place-content-center tw-mt-40 tw-bg-white  ">
-            <Card className="tw-w-96 tw-rounded-lg  tw-bg-white tw-text-center tw-p-10">
-                <b>Login or Signup</b>
+            <Card className="tw-w-96 tw-rounded-lg  tw-bg-white tw-text-center tw-p-10 tw-border-white ">
+                <b className="tw-text-2xl">Login or Signup</b>
                 <Form.Control
                 className="tw-grid tw-place-content-center tw-mt-6"
                 placeholder="Email"
@@ -17,16 +17,16 @@ export default function Home() {
                 aria-describedby="basic-addon1"
                  />
                 <Link href="/dashboard">
-                    <Button className="tw-m-3 tw-bg-slate-800 tw-w-72">Login</Button>
+                    <Button className="tw-m-3 tw-bg-slate-800 tw-border-white tw-w-72">Login</Button>
                 </Link>
  
                     
                         <Link href="/dashboard">
-                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-w-72">With Google</Button>
+                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-border-white tw-w-72">With Google</Button>
                 </Link>
 
                 <Link href="/dashboard">
-                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-w-72">With GitHub</Button>
+                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-border-white tw-w-72">With GitHub</Button>
                 </Link>
             </Card>
         </div>
