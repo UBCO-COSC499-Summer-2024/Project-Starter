d11401d6048187f95136aec4bae5f9064d61439e
Marshall <wguo6358@gmail.com>
2024-06-27 22:45:26 +0000

    edit as csv finished

--- app/cmps_system/next-app/src/app/courses/page.tsx
+++ app/cmps_system/next-app/src/app/courses/page.tsx
@@ -1,12 +1,15 @@
+// this file uses copilot auto compleet in all around areas
 'use client'
-import AddIcon from '@mui/icons-material/Add';
+import { useRouter } from 'next/navigation';
 import Container from 'react-bootstrap/Container';
+import { csv2json, json2csv } from 'json-2-csv';
 import Navbar from "@/app/components/NavBar"
 import { createClient } from '@supabase/supabase-js'
 import Link from 'next/link';
 import Image from 'next/image';
-import { Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Form, FormControl, FormLabel, Modal, ModalBody, ModalDialog, ModalFooter, ModalHeader, ModalTitle, NavDropdown, NavLink, NavbarCollapse, NavbarText, Row, Table } from "react-bootstrap";
-import {Button} from '@mui/material';
+import { Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Form, FormControl, FormGroup, FormLabel, NavDropdown, NavLink, NavbarCollapse, NavbarText, Row, Table } from "react-bootstrap";
+import { Button, Modal, Typography, Box, styled } from '@mui/material';
+import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -16,7 +19,7 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { DataGrid, GridSlots, GridToolbarContainer } from '@mui/x-data-grid';
 import React from "react";
 
@@ -54,21 +57,87 @@ export default function Home() {
     const [courseData, setCourseData] = useState([
         { id: 0, course_name: "COSC 400", location: "FIP 123", instructor: "Professor Dingus", num_students: 120, num_TAs: 4, average_grade: 70, year_level: 4, session: "2021W" },
     ]);
+    const { push } = useRouter();
+    const [defaultCSV, setDefaultCSV] = useState("")
     const EditToolbar = () => {
         return (
             <GridToolbarContainer>
-                <Button color="primary" onClick={()=>{alert}}>
-                ➕ Add record
+                <Button color="primary" onClick={() => { push("/courses/create_new_course") }}>
+                    ➕ Add record
                 </Button>
 
-                <Button color="primary" onClick={()=>{alert}}>
+                <Button color="primary" onClick={() => {
+                    // csv.current.value=(json2csv(courseData))
+                    setDefaultCSV(json2csv(courseData))
+                    setCsvShow(true)
+                }}>
                     ✏️ Edit As CSV
                 </Button>
             </GridToolbarContainer>
         )
     }
-    const renderTable = () => {
-        return (
+
+    const [csvShow, setCsvShow] = useState(false)
+    const handleCSVClose = () => setCsvShow(false);
+    const blue = {
+        100: '#DAECFF',
+        200: '#b6daff',
+        400: '#3399FF',
+        500: '#007FFF',
+        600: '#0072E5',
+        900: '#003A75',
+      };
+      
+      const grey = {
+        50: '#F3F6F9',
+        100: '#E5EAF2',
+        200: '#DAE2ED',
+        300: '#C7D0DD',
+        400: '#B0B8C4',
+        500: '#9DA8B7',
+        600: '#6B7A90',
+        700: '#434D5B',
+        800: '#303740',
+        900: '#1C2025',
+      };
+      
+    const TextareaAutosize = styled(BaseTextareaAutosize)(
+        ({ theme }) => `
+        box-sizing: border-box;
+        width: 100%;
+        font-family: 'IBM Plex Sans', sans-serif;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1.5;
+        padding: 8px 12px;
+        border-radius: 8px;
+        color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+        background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+        border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+        box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+      
+        &:hover {
+          border-color: ${blue[400]};
+        }
+      
+        &:focus {
+          border-color: ${blue[400]};
+          box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+        }
+      
+        // firefox
+        &:focus-visible {
+          outline: 0;
+        }
+      `,
+      );
+    const csv = useRef(null);
+    return (
+        <main>
+
+
+            <Navbar />
+
             <Container>
                 <Row className="h-32">
                     <div className="tw-p-3">
@@ -77,24 +146,42 @@ export default function Home() {
                             rows={courseData}
                             columns={tableColumns}
                             pageSizeOptions={[10000]}
-                            slots={{toolbar: EditToolbar as GridSlots['toolbar']}}
+                            slots={{ toolbar: EditToolbar as GridSlots['toolbar'] }}
                         />
                     </div>
                 </Row>
             </Container>
-        );
-    }
-    return (
-        <main>
-            <Navbar />
-            <span style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-                <h1 style={{ marginRight: "10px" }}>Courses</h1>
-                <Link href="/courses/create_new_course" style={{ display: "flex", alignItems: "center", margin: "0 3em", fontSize: "1.5em" }}>
-                    <Image src="/plus.svg" alt="Add new course plus icon" width={20} height={20} style={{ margin: '20px' }} />
-                    Create new course
-                </Link>
-            </span>
-            {renderTable()}
+
+            <Modal open={csvShow} onClose={handleCSVClose}>
+                <Box sx={{
+                    position: 'absolute' as 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: "80%",
+                    bgcolor: 'background.paper',
+                    boxShadow: 24,
+                    p: 4,
+                }}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Batch Editing
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <TextareaAutosize defaultValue={defaultCSV} ref={csv}></TextareaAutosize>
+                </Typography>
+
+
+
+                <Button className="!tw-m-2" variant="outlined" onClick={handleCSVClose}>Discard</Button>
+                <Button className="!tw-m-2" variant="contained" onClick={() => {
+                    const csvText = csv.current.value;
+                    setCourseData(csv2json(csvText))
+                    handleCSVClose()
+                }}
+
+                >Add</Button>
+            </Box>
+        </Modal>
         </main >
     );
 }
