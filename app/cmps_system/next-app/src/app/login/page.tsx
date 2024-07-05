b3d900727f2f51a185d9356c0cf1d7d92671ad3a
Marshall <wguo6358@gmail.com>
2024-07-05 02:08:51 +0000

    finished login, reember to update .env

--- app/cmps_system/next-app/src/app/login/page.tsx
+++ app/cmps_system/next-app/src/app/login/page.tsx
@@ -1,38 +1,37 @@
+'use client'
+// import by copilot
 import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Link from 'next/link';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { createClient } from '@supabase/supabase-js'
+import { useRef } from "react"; 
 
 export default function Home() {
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_URL, process.env.NEXT_PUBLIC_ANON_KEY);
+    const email_input = useRef(null)
+    async function submit(){
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: 'github',
+          })
+    }
     return (
         <div className="tw-grid tw-place-content-center tw-mt-40 tw-bg-white  ">
-            <Card className="tw-w-96 tw-rounded-lg  tw-bg-white tw-text-center tw-p-10 tw-border-white ">
+            <Card className="tw-w-96 tw-rounded-lg  tw-bg-white tw-text-center tw-p-10 tw-border-white tw-place-content-center">
                 <b className="tw-text-2xl">Login or Signup</b>
-                <Form.Control
-                className="tw-grid tw-place-content-center tw-mt-6"
+                {/* <Form.Control
+                className="tw-grid tw-mt-6 tw-m-3"
                 placeholder="Email"
                 aria-label="email"
                 type="email"
                 aria-describedby="basic-addon1"
-                 />
-                <Link href="/dashboard">
-                    <Button className="tw-m-3 tw-bg-slate-800 tw-border-white tw-w-72">Login/Sign Up</Button>
-                </Link>
- 
-                    
-                        <Link href="/dashboard">
-                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-border-white tw-w-72">With Google</Button>
-                </Link>
-
-                <Link href="/dashboard">
-
-                    <Button className="tw-m-3">With GitHub</Button>
-                    <Button className="tw-m-3 tw-bg-neutral-200 tw-text-slate-950 tw-border-white tw-w-72">With GitHub</Button>
-                </Link>
+                ref={email_input}
+                 /> */}
+                <Button className="tw-m-3 tw-bg-slate-800 tw-border-white" onClick={submit}>With GitHub</Button>
             </Card>
         </div>
     )
 }
 
-  
+
