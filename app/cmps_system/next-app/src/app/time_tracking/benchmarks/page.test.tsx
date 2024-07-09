8f86cfc64fd1afe3f76a32026378999b588e1d01
Marshall <wguo6358@gmail.com>
2024-07-09 01:00:43 +0000

    add test for non exists table

--- app/cmps_system/next-app/src/app/time_tracking/benchmarks/page.test.tsx
+++ app/cmps_system/next-app/src/app/time_tracking/benchmarks/page.test.tsx
@@ -2,7 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.SUPABASE_PUBLIC_URL, process.env.ANON_KEY);
 test('read_from_supabase_time_tracking', async () => {
-    const { data, error } = await supabase.from("v_benchmark").select();
-    expect(data).not.toBeNull()
+    const res = await supabase.from("v_benchmark").select();
+    expect(res.data).not.toBeNull()
+    expect(res.error).toBeNull()
  
+    const res2= await supabase.from("v_benchmark_non_exists_table").select();
+    expect(res2.error).not.toBeNull()
+
   });
