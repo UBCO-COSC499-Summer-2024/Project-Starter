f913172f854901732a28523085193d0b8f5135e2
YIYANG DU <112981522+duyiyang@users.noreply.github.com>
2024-07-16 23:38:50 +0000

    course test finished

---
+++ app/cmps_system/next-app/__test__/course.test.tsx
@@ -0,0 +1,52 @@
+import { createClient } from '@supabase/supabase-js'
+ 
+const supabase = createClient(process.env.SUPABASE_PUBLIC_URL, process.env.ANON_KEY);
+test('read_from_supabase_time_tracking', async () => {
+    const { data, error } = await supabase.from("course").select();
+    expect(data).not.toBeNull()
+    expect(error).toBeNull()
+ 
+  })
+  test('read_from_no_exsist_supabase_time_tracking', async () => {
+    const { data, error } = await supabase.from("no_exsist_course").select();
+    expect(error).not.toBeNull()
+    expect(data).toBeNull()
+ 
+  })
+  test('read_from_no_exsist_supabase_time_tracking', async () => {
+    const { data, error } = await supabase.from("no_exsist_course").select();
+    expect(error).not.toBeNull()
+    expect(data).toBeNull()
+ 
+  })
+  test('read_from_supabase_delete_and_add_benchmark', async () => {
+    const res = await supabase.from("course").select();
+    console.log(res.data)
+    expect(res.error).toBeNull()
+  })
+  test('read_from_supabase_delete_and_add_benchmark', async () => {
+    const res = await supabase.from("course").select();
+    console.log(res.data)
+    expect(res.error).toBeNull()
+  
+  
+    const response = await supabase
+      .from('course')
+      .delete()
+      .eq('course_id', res.data[0].course_id)
+    const res2 = await supabase.from("course").select();
+    expect(res2.error).toBeNull()
+    expect(res2.data.length - res.data.length).toBe(-1)
+  
+    const { error } = await supabase
+      .from('course')
+      .insert(res.data[0])
+    expect(error).toBeNull()
+  
+    const res3 = await supabase.from("course").select();
+  
+    expect(res3.data.length - res.data.length).toBe(0)
+    console.log(res3.data)
+    expect(res3.data.map((x)=>{return x.course_id})).toContain(res.data[0].course_id)
+  
+})
