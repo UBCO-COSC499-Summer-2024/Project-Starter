8701eb9518b3f60485134883cd0b355f8f203c46
YIYANG DU <112981522+duyiyang@users.noreply.github.com>
2024-07-17 11:35:00 -0700

    Update event.test.tsx

--- app/cmps_system/next-app/__test__/event.test.tsx
+++ app/cmps_system/next-app/__test__/event.test.tsx
@@ -1,30 +1,30 @@
 import { createClient } from '@supabase/supabase-js'
  
 const supabase = createClient(process.env.SUPABASE_PUBLIC_URL, process.env.ANON_KEY);
-test('read_from_supabase_time_tracking', async () => {
+test('read_from_supabase_event', async () => {
     const { data, error } = await supabase.from("event").select();
     expect(data).not.toBeNull()
     expect(error).toBeNull()
  
   })
-  test('read_from_no_exsist_supabase_time_tracking', async () => {
+  test('read_from_no_exsist_supabase_event', async () => {
     const { data, error } = await supabase.from("no_exsist_event").select();
     expect(error).not.toBeNull()
     expect(data).toBeNull()
  
   })
-  test('read_from_no_exsist_supabase_time_tracking', async () => {
+  test('read_from_no_exsist_supabase_event', async () => {
     const { data, error } = await supabase.from("no_exsist_event").select();
     expect(error).not.toBeNull()
     expect(data).toBeNull()
  
   })
-  test('read_from_supabase_delete_and_add_benchmark', async () => {
+  test('read_from_supabase_delete_and_add_event', async () => {
     const res = await supabase.from("event").select();
     console.log(res.data)
     expect(res.error).toBeNull()
   })
-  test('read_from_supabase_delete_and_add_benchmark', async () => {
+  test('read_from_supabase_delete_and_add_event', async () => {
     const res = await supabase.from("event").select();
     console.log(res.data)
     expect(res.error).toBeNull()
