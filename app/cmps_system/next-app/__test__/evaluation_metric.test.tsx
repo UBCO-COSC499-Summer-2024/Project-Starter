f9b107abef6b305450fc1aced0e9c4b242ce149f
YIYANG DU <112981522+duyiyang@users.noreply.github.com>
2024-07-21 03:58:49 +0000

    evaluation metric test finished

---
+++ app/cmps_system/next-app/__test__/evaluation_metric.test.tsx
@@ -0,0 +1,20 @@
+import { createClient } from '@supabase/supabase-js'
+ 
+const supabase = createClient(process.env.SUPABASE_PUBLIC_URL, process.env.ANON_KEY);
+test('read_from_supabase_evaluation_metric', async () => {
+    const { data, error } = await supabase.from("evaluation_metric").select();
+    expect(data).not.toBeNull()
+    expect(error).toBeNull()
+ 
+  })
+  test('read_from_no_exsist_supabase_evaluation_metric', async () => {
+    const { data, error } = await supabase.from("no_exsist_evaluation_metric").select();
+    expect(error).not.toBeNull()
+    expect(data).toBeNull()
+ 
+  })
+  test('read_from_supabase_delete_and_add_evaluation_metric', async () => {
+    const res = await supabase.from("evaluation_metric").select();
+    console.log(res.data)
+    expect(res.error).toBeNull()
+  })
