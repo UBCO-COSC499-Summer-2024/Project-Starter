4daebe9952085a32ad6abaadd38421482fc5e7ee
YIYANG DU <112981522+duyiyang@users.noreply.github.com>
2024-07-21 03:53:53 +0000

    evaluation entry test finished

--- app/cmps_system/next-app/__test__/course.test.tsx
+++ app/cmps_system/next-app/__test__/course.test.tsx
@@ -13,40 +13,8 @@ test('read_from_supabase_course', async () => {
     expect(data).toBeNull()
  
   })
-  test('read_from_no_exsist_supabase_course', async () => {
-    const { data, error } = await supabase.from("no_exsist_course").select();
-    expect(error).not.toBeNull()
-    expect(data).toBeNull()
- 
-  })
   test('read_from_supabase_delete_and_add_course', async () => {
     const res = await supabase.from("course").select();
     console.log(res.data)
     expect(res.error).toBeNull()
   })
-  test('read_from_supabase_delete_and_add_course', async () => {
-    const res = await supabase.from("course").select();
-    console.log(res.data)
-    expect(res.error).toBeNull()
-  
-  
-    const response = await supabase
-      .from('course')
-      .delete()
-      .eq('course_id', res.data[0].course_id)
-    const res2 = await supabase.from("course").select();
-    expect(res2.error).toBeNull()
-    expect(res2.data.length - res.data.length).toBe(-1)
-  
-    const { error } = await supabase
-      .from('course')
-      .insert(res.data[0])
-    expect(error).toBeNull()
-  
-    const res3 = await supabase.from("course").select();
-  
-    expect(res3.data.length - res.data.length).toBe(0)
-    console.log(res3.data)
-    expect(res3.data.map((x)=>{return x.course_id})).toContain(res.data[0].course_id)
-  
-})
