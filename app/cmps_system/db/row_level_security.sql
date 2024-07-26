26e28332e528dafd6cc6c2df82cce5d6c0b5b61b
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-07-25 17:24:57 -0700

    Made RLS policies for instructor table. Gave the default staff and head emails to two instructors

--- app/cmps_system/db/row_level_security.sql
+++ app/cmps_system/db/row_level_security.sql
@@ -6,10 +6,6 @@ CREATE POLICY "select_all_instructors" ON public.instructor FOR
 SELECT
     TO authenticated USING (true);
 
--- Write access for everyone
-CREATE POLICY "update_instructors" ON public.instructor FOR
-UPDATE TO authenticated USING (true);
-
 -- Insert access for staff and head
 CREATE POLICY "insert_instructors" ON public.instructor FOR INSERT TO authenticated
 WITH
@@ -23,3 +19,39 @@ WITH
                 user_id = auth.uid ()
         ) IN ('head', 'staff')
     );
+
+-- Update access for staff and head
+CREATE POLICY "update_instructors" ON public.instructor FOR
+UPDATE TO authenticated USING (
+    (
+        SELECT
+            role
+        FROM
+            public.user_role
+        WHERE
+            user_id = auth.uid ()
+    ) IN ('head', 'staff')
+)
+WITH
+    CHECK (
+        (
+            SELECT
+                role
+            FROM
+                public.user_role
+            WHERE
+                user_id = auth.uid ()
+        ) IN ('head', 'staff')
+    );
+
+-- Delete access for staff and head
+CREATE POLICY "delete_instructors" ON public.instructor FOR DELETE TO authenticated USING (
+    (
+        SELECT
+            role
+        FROM
+            public.user_role
+        WHERE
+            user_id = auth.uid ()
+    ) IN ('head', 'staff')
+)
