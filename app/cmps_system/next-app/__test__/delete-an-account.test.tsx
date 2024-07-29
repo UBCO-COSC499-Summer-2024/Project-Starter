c8d348d2e30fabaefa1e559efda7e6fae401fffd
jlgsl <88369347+jlgsl@users.noreply.github.com>
2024-07-29 16:37:07 -0700

    Update delete-an-account.test.tsx

--- app/cmps_system/next-app/__test__/delete-an-account.test.tsx
+++ app/cmps_system/next-app/__test__/delete-an-account.test.tsx
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Mock the Supabase client
 const supabaseUrl = 'http://localhost:8000'; // Replace with actual URL if needed
-const supabaseServiceKey = 'service-role-key';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q';
 
 const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
 
