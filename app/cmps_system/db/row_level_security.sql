e95849034eb1395fceecc3938d1be4c6ebb82d19
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-07-30 01:16:06 -0700

    Changed all ANON_KEY usages to SERVICE_ROLE_KEY in the test scripts. Added RLS for service_role

--- app/cmps_system/db/row_level_security.sql
+++ app/cmps_system/db/row_level_security.sql
@@ -55,3 +55,61 @@ CREATE POLICY "delete_instructors" ON public.instructor FOR DELETE TO authentica
             user_id = auth.uid ()
     ) IN ('head', 'staff')
 )
+
+-- service_role table
+ALTER TABLE public.service_role ENABLE ROW LEVEL SECURITY;
+
+-- Read access for everyone
+CREATE POLICY "select_all_service_roles" ON public.service_role FOR
+SELECT
+    TO authenticated USING (true);
+
+-- Insert access for staff and head
+CREATE POLICY "insert_service_roles" ON public.service_role FOR INSERT TO authenticated
+WITH
+    CHECK (
+        (
+            SELECT
+                role
+            FROM
+                public.user_role
+            WHERE
+                user_id = auth.uid ()
+        ) IN ('head', 'staff')
+    );
+
+-- Update access for staff and head
+CREATE POLICY "update_service_roles" ON public.service_role FOR
+UPDATE TO authenticated USING (
+    (
+        SELECT
+            role
+        FROM
+            public.user_role
+        WHERE
+            user_id = auth.uid ()
+    ) IN ('head', 'staff')
+)
+WITH
+    CHECK (
+        (
+            SELECT
+                role
+            FROM
+                public.user_role
+            WHERE
+                user_id = auth.uid ()
+        ) IN ('head', 'staff')
+    );
+
+-- Delete access for staff and head
+CREATE POLICY "delete_service_roles" ON public.service_role FOR DELETE TO authenticated USING (
+    (
+        SELECT
+            role
+        FROM
+            public.user_role
+        WHERE
+            user_id = auth.uid ()
+    ) IN ('head', 'staff')
+)
