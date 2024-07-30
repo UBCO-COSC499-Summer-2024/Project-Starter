c3076248bab5e5cbd53e729daf29ff5bdc1d61be
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-07-29 19:22:41 -0700

    Added singular id field to 'evaluation_metric' for convenience, adjusted evaluation_metric.test.tsx

--- app/cmps_system/next-app/__test__/evaluation_metric.test.tsx
+++ app/cmps_system/next-app/__test__/evaluation_metric.test.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const testData = {
+  evaluation_metric_id: -3,
   evaluation_type_id: 3,
   metric_num: 10000,
   metric_description: 'Overall satisfaction with the course',
@@ -32,18 +33,18 @@ test('add a row', async () => {
   expect(error).toBeNull()
   const res2 = await supabase.from("evaluation_metric").select();
   expect(res2.data.length - res.data.length).toBe(1) //check if a row really got added
-  expect(res2.data.map((x) => { return x.evaluation_type_id })).toContain(testData.evaluation_type_id) //to see if the data is really there
+  expect(res2.data.map((x) => { return x.evaluation_metric_id })).toContain(testData.evaluation_metric_id) //to see if the data is really there
 })
 test('delete a row', async () => {
   const res = await supabase.from("evaluation_metric").select();
   const { error } = await supabase
     .from('evaluation_metric')
     .delete()
-    .eq('evaluation_type_id', testData.evaluation_type_id)
+    .eq('evaluation_metric_id', testData.evaluation_metric_id)
   expect(error).toBeNull()
   const res2 = await supabase.from("evaluation_metric").select();
   expect(res2.data.length - res.data.length).toBe(-1) //check if a row really got deleted
-  expect(res2.data.map((x) => { return x.evaluation_type_id })).not.toContain(testData.evaluation_type_id) //to see if the data is really deleted
+  expect(res2.data.map((x) => { return x.evaluation_metric_id })).not.toContain(testData.evaluation_metric_id) //to see if the data is really deleted
 })
 
 
