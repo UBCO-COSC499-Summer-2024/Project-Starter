0142544755e84cfa97b77c6ebacd7ed395fa27dc
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-07-30 05:10:34 -0700

    Fixed SearchModals on time tracking page

--- app/cmps_system/next-app/src/app/time_tracking/page.tsx
+++ app/cmps_system/next-app/src/app/time_tracking/page.tsx
@@ -1,33 +1,33 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Button } from '@mui/material';
-import Navbar from "@/app/components/NavBar";
-import supabase from "@/app/components/supabaseClient";
+'use client'
+import React from 'react';
 import CMPS_Table from '@/app/components/CMPS_Table';
+import supabase from "@/app/components/supabaseClient";
+import Navbar from "@/app/components/NavBar";
+import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
-export default function TimeTracking() {
+export default function Evaluations() {
     const router = useRouter();
-
     const fetchUrl = "v_timetracking";
     const tableName = "service_hours_entry";
     const initialSortModel = [
-        { field: 'instructor_name', sort: 'asc' },
+        { field: 'id', sort: 'desc' },
     ];
 
     const columnsConfig = [
+        { field: 'id', headerName: 'ID', width: 75, editable: false },
         {
-            field: 'instructor_name',
+            field: 'instructor_full_name',
             headerName: 'Instructor',
             flex: 1,
-            editConfig: { type: 'searchModal', modalType: 'instructor', canEdit: () => true },
+            editConfig: { type: 'searchModal', modalType: 'instructor' },
             linkConfig: { prefix: '/instructors/instructor_info?id=', idField: 'instructor_id' }
         },
         {
-            field: 'service_role_name',
+            field: 'service_role',
             headerName: 'Service Role',
             flex: 1,
-            editConfig: { type: 'searchModal', modalType: 'service_role', canEdit: () => true },
+            editConfig: { type: 'searchModal', modalType: 'service_role' },
             linkConfig: { prefix: '/service_roles/service_role_info?id=', idField: 'service_role_id' }
         },
         { field: 'year', headerName: 'Year', flex: 1, editable: true },
@@ -41,19 +41,20 @@ export default function TimeTracking() {
             .upsert({
                 service_hours_entry_id: row.id,
                 instructor_id: row.instructor_id,
+                service_role_id: row.service_role_id,
                 year: row.year,
                 hours: row.hours,
-                month: row.month,
-                service_role_id: row.service_role_id
+                month: row.month
             });
 
         if (error) {
-            return { error };
+            return { error }; // Ensure error is returned here
         }
     };
 
+
     return (
-        <main>
+        <>
             <Navbar />
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
                 <h1 style={{ marginRight: "10px" }}>Time Tracking</h1>
@@ -66,16 +67,15 @@ export default function TimeTracking() {
                     </Button>
                 </div>
             </div>
-
             <CMPS_Table
                 fetchUrl={fetchUrl}
                 columnsConfig={columnsConfig}
                 initialSortModel={initialSortModel}
                 tableName={tableName}
                 rowUpdateHandler={rowUpdateHandler}
+                deleteWarningMessage="Are you sure you want to delete this evaluation?"
                 idColumn="service_hours_entry_id"
-                deleteWarningMessage="Are you sure you want to delete the selected time entries? This action is not recoverable!"
             />
-        </main>
+        </>
     );
 }
