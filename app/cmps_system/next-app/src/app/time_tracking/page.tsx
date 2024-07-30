a3d2d2f0a1c874a4d7b7fa05150551bdd4c73d06
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-07-30 04:26:17 -0700

    Saving progress. still not working

--- app/cmps_system/next-app/src/app/time_tracking/page.tsx
+++ app/cmps_system/next-app/src/app/time_tracking/page.tsx
@@ -5,57 +5,9 @@ import { Button } from '@mui/material';
 import Navbar from "@/app/components/NavBar";
 import supabase from "@/app/components/supabaseClient";
 import CMPS_Table from '@/app/components/CMPS_Table';
-import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
-import { styled } from '@mui/material/styles';
-
-const TextareaAutosize = styled(BaseTextareaAutosize)(
-    ({ theme }) => `
-    box-sizing: border-box;
-    width: 100%;
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: 8px 12px;
-    border-radius: 8px;
-    color: ${theme.palette.mode === 'dark' ? '#DAE2ED' : '#303740'};
-    background: ${theme.palette.mode === 'dark' ? '#1C2025' : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? '#6B7A90' : '#DAE2ED'};
-    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? '#1C2025' : '#F3F6F9'};
-    &:hover {
-      border-color: '#3399FF';
-    }
-    &:focus {
-      border-color: '#3399FF';
-      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? '#0072E5' : '#b6daff'};
-    }
-    &:focus-visible {
-      outline: 0;
-    }
-  `,
-);
 
 export default function TimeTracking() {
     const router = useRouter();
-    const [instructors, setInstructors] = useState([]);
-    const [serviceRoles, setServiceRoles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const ins = await supabase.from("list_of_instructors").select();
-                const ser_roles = await supabase.from("list_all_service_roles").select();
-                setServiceRoles(ser_roles.data.map((service_role) => service_role.service_role_name));
-                setInstructors(ins.data.map((instructor) => instructor.name));
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        })();
-    }, []);
 
     const fetchUrl = "v_timetracking";
     const tableName = "service_hours_entry";
@@ -68,14 +20,14 @@ export default function TimeTracking() {
             field: 'instructor_name',
             headerName: 'Instructor',
             flex: 1,
-            editConfig: { type: 'searchModal', modalType: 'instructor' },
+            editConfig: { type: 'searchModal', modalType: 'instructor', canEdit: () => true },
             linkConfig: { prefix: '/instructors/instructor_info?id=', idField: 'instructor_id' }
         },
         {
             field: 'service_role_name',
             headerName: 'Service Role',
             flex: 1,
-            editConfig: { type: 'searchModal', modalType: 'service_role' },
+            editConfig: { type: 'searchModal', modalType: 'service_role', canEdit: () => true },
             linkConfig: { prefix: '/service_roles/service_role_info?id=', idField: 'service_role_id' }
         },
         { field: 'year', headerName: 'Year', flex: 1, editable: true },
@@ -115,21 +67,15 @@ export default function TimeTracking() {
                 </div>
             </div>
 
-            {loading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>Error: {error}</p>
-            ) : (
-                <CMPS_Table
-                    fetchUrl={fetchUrl}
-                    columnsConfig={columnsConfig}
-                    initialSortModel={initialSortModel}
-                    tableName={tableName}
-                    rowUpdateHandler={rowUpdateHandler}
-                    idColumn="service_hours_entry_id"
-                    deleteWarningMessage="Are you sure you want to delete the selected time entries? This action is not recoverable!"
-                />
-            )}
+            <CMPS_Table
+                fetchUrl={fetchUrl}
+                columnsConfig={columnsConfig}
+                initialSortModel={initialSortModel}
+                tableName={tableName}
+                rowUpdateHandler={rowUpdateHandler}
+                idColumn="service_hours_entry_id"
+                deleteWarningMessage="Are you sure you want to delete the selected time entries? This action is not recoverable!"
+            />
         </main>
     );
 }
