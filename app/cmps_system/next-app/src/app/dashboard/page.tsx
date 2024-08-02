ce07a732888807944afa5f3a1f797af94038d8bb
GrandTheftWalrus <vaughn.janes@gmail.com>
2024-08-01 23:52:50 -0700

    Code cleanup

--- app/cmps_system/next-app/src/app/dashboard/page.tsx
+++ app/cmps_system/next-app/src/app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-'use client'
-import Image from "next/image";
+'use client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Card, Table, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ProgressBar } from 'react-bootstrap';
+import supabase from "@/app/components/supabaseClient";
 import Navbar from '@/app/components/NavBar';
-import { createClient } from '@supabase/supabase-js'
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
-import Button from 'react-bootstrap/Button';
-import Card from "react-bootstrap/esm/Card";
-import { Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, NavDropdown, NavLink, NavbarCollapse, NavbarText, ProgressBar, Row, Table } from "react-bootstrap";
+import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -16,11 +12,8 @@ import {
     BarElement,
     Title,
     Tooltip,
-    Legend,
+    Legend
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import { useState, useEffect } from "react";
-import supabase from "@/app/components/supabaseClient";
 
 ChartJS.register(
     CategoryScale,
@@ -31,36 +24,18 @@ ChartJS.register(
     Legend
 );
 
-const parseData = function (x) {
-
-    return {
-        labels: x.map(entry => entry.month),
-        datasets: [{
-            label: 'Personal Monthly Working Hours',
-            data: x.map(entry => entry.hours),
-            backgroundColor: [
-                'rgba(58, 190, 249, 0.2)',
-            ],
-            borderColor: [
-                'rgb(58, 190, 249)',
-            ],
-            borderWidth: 1
-        }]
-    }
-}
+const parseData = (x) => ({
+    labels: x.map(entry => entry.month),
+    datasets: [{
+        label: 'Personal Monthly Working Hours',
+        data: x.map(entry => entry.hours),
+        backgroundColor: 'rgba(58, 190, 249, 0.2)',
+        borderColor: 'rgb(58, 190, 249)',
+        borderWidth: 1
+    }]
+});
 
 const HourCard = () => {
-    /** This function render the hour card showing how many hours the user and department worked vs expected. It query a database view that is quiet complex 
-     * with many sub queries to achrive this.
-     * ```
-     * CREATE OR REPLACE VIEW progress AS 
-     *  SELECT instructor.email, hours.instructor_id, hours.worked, hours.expected FROM (SELECT worked.instructor_id, 
-     *       worked.sum AS worked, expected.sum AS expected FROM (select instructor_id, SUM(hours) from service_hours_entry GROUP 
-     *       BY instructor_id) AS worked JOIN (select instructor_id, sum(hours) from service_hours_benchmark group by instructor_id)
-     *       as expected ON worked.instructor_id=expected.instructor_id) AS hours JOIN instructor ON hours.instructor_id=instructor.instructor_id;
-     * ```
-     * It uses the data to render a number showing and a progress bar.
-     */
     const [personalHour, setPersonalHour] = useState(0);
     const [departmentHour, setDepartmentHour] = useState(0);
     const [personalExpected, setPersonalExpected] = useState(0);
@@ -96,7 +71,7 @@ const HourCard = () => {
         })();
     }, []);
 
-    return <>
+    return (
         <Card className="tw-mb-3">
             <div style={{ color: personalExpected === 0 || personalHour / personalExpected > 0.5 ? "green" : "orange" }}>
                 <b className="tw-mt-2 tw-ml-2 tw-text-lg">Personal service hours this month</b>
@@ -112,11 +87,10 @@ const HourCard = () => {
                 <ProgressBar className="tw-m-3" now={departmentExpected === 0 ? 0 : departmentHour / departmentExpected * 100} />
             </div>
         </Card>
-    </>
+    );
+};
 
-}
 export default function Home() {
-
     const [term, setTerm] = useState("2024");
     const [workingHours, setWorkingHours] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -130,33 +104,12 @@ export default function Home() {
     const [ratingTerm, setRatingTerm] = useState("2024");
     const [rating, setRating] = useState({
         "2023": [
-
-            {
-                name: "COSC101",
-                term: "1",
-                student_count: 100,
-                rating: "88%"
-            },
-            {
-                name: "COSC121",
-                term: "2",
-                student_count: 55,
-                rating: "50%"
-            },
+            { name: "COSC101", term: "1", student_count: 100, rating: "88%" },
+            { name: "COSC121", term: "2", student_count: 55, rating: "50%" }
         ],
         "2024": [
-            {
-                name: "COSC101",
-                term: "1",
-                student_count: 120,
-                rating: "98%"
-            },
-            {
-                name: "COSC121",
-                term: "2",
-                student_count: 65,
-                rating: "30%"
-            },
+            { name: "COSC101", term: "1", student_count: 120, rating: "98%" },
+            { name: "COSC121", term: "2", student_count: 65, rating: "30%" }
         ]
     });
 
@@ -218,11 +171,9 @@ export default function Home() {
         fetchWorkingHours();
     }, [term]);
 
-
     return (
         <main>
             <Navbar />
-
             <Container>
                 <Row className="tw-pt-3">
                     <Col xs={8}>
@@ -239,12 +190,10 @@ export default function Home() {
                                     </tr>
                                 </thead>
                                 <tbody>
-
                                     {assignmentsLoading ? (
                                         <tr><td colSpan="5">Loading...</td></tr>
                                     ) : assignmentsError ? (
                                         <tr><td colSpan="5">Error fetching assignments: {assignmentsError}</td></tr>
-
                                     ) : assignments.length > 0 ? (
                                         assignments.map((x, index) => (
                                             <tr key={index}>
@@ -266,7 +215,6 @@ export default function Home() {
                         <HourCard />
                         <Card>
                             <b className="tw-mt-2 tw-ml-2 tw-text-lg">Hours per service role this month</b>
-
                             <Table>
                                 <thead>
                                     <tr>
@@ -299,16 +247,13 @@ export default function Home() {
                         <Card className="tw-mb-3">
                             <b className="tw-mt-2 tw-ml-2 tw-text-lg">Historical Working Hours</b>
                             <Dropdown className="tw-ml-2">
-                                <DropdownToggle>
-                                    {term}
-                                </DropdownToggle>
+                                <DropdownToggle>{term}</DropdownToggle>
                                 <DropdownMenu>
                                     {[2023, 2024].map((year, index) => (
-                                        <DropdownItem key={index} onClick={() => {
-                                            setTerm(year)
-                                        }}>
+                                        <DropdownItem key={index} onClick={() => setTerm(year)}>
                                             {year}
-                                        </DropdownItem>))}
+                                        </DropdownItem>
+                                    ))}
                                 </DropdownMenu>
                             </Dropdown>
                             <div className="tw-h-62">
@@ -316,11 +261,7 @@ export default function Home() {
                                     data={parseData(workingHours)}
                                     options={{
                                         maintainAspectRatio: false,
-                                        scales: {
-                                            y: {
-                                                beginAtZero: true
-                                            }
-                                        },
+                                        scales: { y: { beginAtZero: true } }
                                     }}
                                 />
                             </div>
@@ -330,16 +271,13 @@ export default function Home() {
                         <Card>
                             <b className="tw-mt-2 tw-ml-2 tw-text-lg">SEI Results</b>
                             <Dropdown className="tw-ml-2">
-                                <DropdownToggle>
-                                    {ratingTerm}
-                                </DropdownToggle>
+                                <DropdownToggle>{ratingTerm}</DropdownToggle>
                                 <DropdownMenu>
                                     {Object.keys(rating).map((x, index) => (
-                                        <DropdownItem key={index} onClick={() => {
-                                            setRatingTerm(x)
-                                        }}>
+                                        <DropdownItem key={index} onClick={() => setRatingTerm(x)}>
                                             {x}
-                                        </DropdownItem>))}
+                                        </DropdownItem>
+                                    ))}
                                 </DropdownMenu>
                             </Dropdown>
                             <Table>
@@ -352,16 +290,14 @@ export default function Home() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {
-                                        rating[ratingTerm].map((x, index) => (
-                                            <tr key={index}>
-                                                <td>{x.name}</td>
-                                                <td>{x.term}</td>
-                                                <td>{x.student_count}</td>
-                                                <td>{x.rating}</td>
-                                            </tr>
-                                        ))
-                                    }
+                                    {rating[ratingTerm].map((x, index) => (
+                                        <tr key={index}>
+                                            <td>{x.name}</td>
+                                            <td>{x.term}</td>
+                                            <td>{x.student_count}</td>
+                                            <td>{x.rating}</td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </Table>
                         </Card>
