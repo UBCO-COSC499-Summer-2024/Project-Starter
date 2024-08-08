eafa6dce9f42b07f5caea02af09d0e800fce9ca8
Marshall <wguo6358@gmail.com>
2024-08-08 06:52:36 +0000

    add comments

--- app/cmps_system/next-app/build.sh
+++ app/cmps_system/next-app/build.sh
@@ -1,4 +1,5 @@
-source ../.env
+# Load environment variables, this is to sync with the docker environment. It makes it easy to run build test on GitHub actions
+source ../.env 
 export NEXT_PUBLIC_SUPABASE_PUBLIC_URL=${SUPABASE_PUBLIC_URL}
 export NEXT_PUBLIC_ANON_KEY=${ANON_KEY}
 export NEXT_PUBLIC_SITE_URL=${SITE_URL}
